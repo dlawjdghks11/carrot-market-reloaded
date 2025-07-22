6c66208ab5229f4213df055b266f682bba141a96
dlawjdghks11 <85840643+dlawjdghks11@users.noreply.github.com>
2025-07-22 14:47:36 +0900

    Add: Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,19 +2,12 @@ import FormInput from "@/components/form-input";
 import FormBtn from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
-const CreateAccount = () => {
+const Login = () => {
   return (
     <div className="relative max-w-lg h-screen ml-auto mr-auto bg-neutral-900 *:text-white px-5 pt-10">
       <h1 className="mb-1">안녕하세요!</h1>
-      <div className="mb-7">Fill in the form below to join!</div>
+      <div className="mb-7">Log in with email and password!</div>
       <div className="flex flex-col gap-1 mb-2">
-        <FormInput
-          type="text"
-          name="text"
-          placeholder="Username"
-          required
-          errorMessage={["Input error"]}
-        />
         <FormInput
           type="email"
           name="email"
@@ -29,19 +22,12 @@ const CreateAccount = () => {
           required
           errorMessage={["Input error"]}
         />
-        <FormInput
-          type="password"
-          name="password"
-          placeholder="Confirm Password"
-          required
-          errorMessage={["Input error"]}
-        />
       </div>
       <FormBtn text="Create account" />
-      <div className="border-b-[1px] border-gray-300 w-full my-4"></div>
+      <div className="border-b-[1px] border-gray-300 w-full my-10"></div>
       <SocialLogin />
     </div>
   );
 };
 
-export default CreateAccount;
+export default Login;
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -19,7 +19,7 @@ const FormInput = ({
         type={type}
         name={name}
         required={required}
-        className="border-gray-100 w-full h-8 ring-1 focus:ring-2 ring-neutral-200 
+        className="border-gray-100 w-full h-10 ring-1 focus:ring-2 ring-neutral-200 
         focus:ring-orange-500 transition-colors rounded-md pl-3 mb-2 focus:outline-none"
         placeholder={placeholder}
       />
